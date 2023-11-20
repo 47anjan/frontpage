f0defb9cc7092d8bfaa4ac6aa6c9a005841906bd
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-20 12:07:00 +0600

    Uninstall EditorJS

--- app/(editor)/editor/[postId]/page.tsx
+++ app/(editor)/editor/[postId]/page.tsx
@@ -1,71 +1,22 @@
 "use client";
-import { postPatchSchema } from "@/lib/validations/post";
-import EditorJS from "@editorjs/editorjs";
+import * as React from "react";
+import Link from "next/link";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRef, useState, useEffect, useCallback } from "react";
 import { useForm } from "react-hook-form";
-import TextareaAutosize from "react-textarea-autosize";
-import { z } from "zod";
-
-const post = {
-  title: "Hello",
-};
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+import { postPatchSchema } from "@/lib/validations/post";
+import { buttonVariants } from "@/components/ui/button";
 
 type FormData = z.infer<typeof postPatchSchema>;
 
-const EditorPage = () => {
+function Editor() {
   const { register, handleSubmit } = useForm<FormData>({
     resolver: zodResolver(postPatchSchema),
   });
 
-  const ref = useRef<EditorJS>();
-
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-
-  const initializeEditor = useCallback(async () => {
-    const EditorJS = (await import("@editorjs/editorjs")).default;
-    const Header = (await import("@editorjs/header")).default;
-
-    const body = postPatchSchema.parse(post);
-
-    if (!ref.current) {
-      const editor = new EditorJS({
-        holder: "editor",
-        onReady() {
-          ref.current = editor;
-        },
-        placeholder: "Type here to write your post...",
-        inlineToolbar: true,
-        data: body.content,
-        tools: {
-          header: Header,
-        },
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setIsMounted(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isMounted) {
-      initializeEditor();
-      return () => {
-        ref.current?.destroy();
-        ref.current = undefined;
-      };
-    }
-  }, [isMounted, initializeEditor]);
-
   async function onSubmit(data: FormData) {
     console.log(data);
-  }
-
-  if (!isMounted) {
-    return null;
   }
 
   return (
@@ -73,35 +24,37 @@ const EditorPage = () => {
       <div className="grid w-full gap-10">
         <div className="flex w-full items-center justify-between">
           <div className="flex items-center space-x-10">
-            <div>Link</div>
+            <Link
+              href="/dashboard"
+              className={cn(buttonVariants({ variant: "ghost" }))}
+            >
+              <>Back</>
+            </Link>
             <p className="text-sm text-muted-foreground">
               {true ? "Published" : "Draft"}
             </p>
           </div>
-          <button type="submit">
+          <button type="submit" className={cn(buttonVariants())}>
             <span>Save</span>
           </button>
         </div>
         <div className="prose prose-stone mx-auto w-[800px] dark:prose-invert">
-          <TextareaAutosize
+          <input
             autoFocus
             id="title"
-            defaultValue={post.title}
             placeholder="Post title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
             {...register("title")}
           />
-          <div id="editor" className="min-h-[500px]" />
-          <p className="text-sm text-gray-500">
-            Use{" "}
-            <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">
-              Tab
-            </kbd>{" "}
-            to open the command menu.
-          </p>
+          <textarea
+            {...register("content")}
+            id="editor"
+            className="min-h-[500px]"
+          />
         </div>
       </div>
     </form>
   );
-};
-export default EditorPage;
+}
+
+export default Editor;
