91aab101866aecf0ef0dfe657d6003446816de0e
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-21 09:26:19 +0600

    Showing post in dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,19 @@
+import { getCurrentUser } from "@/app/auth/sessions";
 import { EmptyPlaceholder } from "@/components/empty-placeholder";
 import { PostCreateButton } from "@/components/post-create-button";
+import { PostItem } from "@/components/post-item";
+import prisma from "@/prisma/client";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const user = await getCurrentUser();
+  if (!user || !user?.email) return null;
+
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: user.email,
+    },
+  });
+
   return (
     <section className="w-full max-w-5xl mx-auto p-4 flex flex-col gap-8">
       <div className="flex items-center justify-between px-2">
@@ -13,14 +25,24 @@ const Dashboard = () => {
         </div>
         <PostCreateButton />
       </div>
-      {/* <div className="divide-y divide-border rounded-md border">grgre</div> */}
-      <EmptyPlaceholder>
-        <EmptyPlaceholder.Title>No posts created</EmptyPlaceholder.Title>
-        <EmptyPlaceholder.Description>
-          You don&apos;t have any posts yet. Start creating content.
-        </EmptyPlaceholder.Description>
-        <PostCreateButton variant="outline" />
-      </EmptyPlaceholder>
+
+      <div>
+        {posts?.length ? (
+          <div className="divide-y divide-border rounded-md border">
+            {posts.map((post) => (
+              <PostItem key={post.id} post={post} />
+            ))}
+          </div>
+        ) : (
+          <EmptyPlaceholder>
+            <EmptyPlaceholder.Title>No posts created</EmptyPlaceholder.Title>
+            <EmptyPlaceholder.Description>
+              You don&apos;t have any posts yet. Start creating content.
+            </EmptyPlaceholder.Description>
+            <PostCreateButton variant="outline" />
+          </EmptyPlaceholder>
+        )}
+      </div>
     </section>
   );
 };
