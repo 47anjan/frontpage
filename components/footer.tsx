5f2c4fd9e0c02942beb155ec0f8441b8869ffb12
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-22 17:13:40 +0600

    Uptade footer information

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,38 +3,20 @@ const Footer = () => {
     <footer>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <div>Logo</div>
+          <h2 className="font-bold">frontpage</h2>
           <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
             <a
-              href={"/"}
+              href={"https://twitter.com/anjanstwt"}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              shadcn
-            </a>
-            . Hosted on{" "}
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . Illustrations by{" "}
-            <a
-              href="https://popsy.co"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Popsy
+              anjan
             </a>
             . The source code is available on{" "}
             <a
-              href={"/"}
+              href={"https://github.com/anjankarmakar10/frontpage"}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
