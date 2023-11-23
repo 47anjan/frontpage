f898b78ca63d1f4294641e387d255596f12bbcd1
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-11-23 11:51:05 +0600

    Add animation on posts page

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,13 +1,21 @@
+"use client";
 import { Post } from "@prisma/client";
 import { PostItem } from "./post-item";
-
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 interface Props {
   posts: Post[];
 }
 
 const Posts = ({ posts }: Props) => {
+  const [animationParent] = useAutoAnimate({
+    easing: "ease-in",
+  });
+
   return (
-    <section className="divide-y divide-border rounded-md border">
+    <section
+      ref={animationParent}
+      className="divide-y divide-border rounded-md border"
+    >
       {posts.map((post) => (
         <PostItem key={post.id} post={post} />
       ))}
